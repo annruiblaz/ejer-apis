da50558c4fb9a952a23b8a1783f900b6fa09797c
anna-ruiz <anna.ruiz.blazquez@cieep.com>
2023-12-11 17:11:49 +0100

    Pruebaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AgentsComponent} from "./pages/agents/agents.component";
+import {CardsComponent} from "./pages/cards/cards.component";
+import {CharactersComponent} from "./pages/characters/characters.component";
+import {WeaponsComponent} from "./pages/weapons/weapons.component";
+import {InicioComponent} from "./pages/inicio/inicio.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'agents',
+    component: AgentsComponent
+  },
+  {
+    path: 'cards',
+    component: CardsComponent
+  },{
+    path: 'characters',
+    component: CharactersComponent
+  },{
+    path: 'weapons',
+    component: WeaponsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { AgentsComponent } from './pages/agents/agents.component';
+import { WeaponsComponent } from './pages/weapons/weapons.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import {HttpClientModule} from "@angular/common/http";
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AgentsComponent,
+    WeaponsComponent,
+    CardsComponent,
+    CharactersComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
